4f2fd25860f46f74d4c8ee12c26a952ee874d1ae
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-01-30 22:38:40 -0300

    create category entity

---
+++ src/main/java/com/marcelodev/movieflix/entity/Category.java
@@ -0,0 +1,21 @@
+package com.marcelodev.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+}
