504f9e7492af488c411748a915347d586bf57ada
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-01-30 22:51:20 -0300

    feat: create category controller

---
+++ src/main/java/com/marcelodev/movieflix/controller/CategoryController.java
@@ -0,0 +1,14 @@
+package com.marcelodev.movieflix.controller;
+
+import com.marcelodev.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/movieflix/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+}
