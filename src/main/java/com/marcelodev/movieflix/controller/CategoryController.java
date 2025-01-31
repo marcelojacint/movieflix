86785cb33b2cc4d1c627aa52b60eac3b56d46092
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-01-31 20:03:48 -0300

    create findall method in service

--- src/main/java/com/marcelodev/movieflix/controller/CategoryController.java
+++ src/main/java/com/marcelodev/movieflix/controller/CategoryController.java
@@ -11,4 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+
 }
--- src/main/java/com/marcelodev/movieflix/service/CategoryService.java
+++ src/main/java/com/marcelodev/movieflix/service/CategoryService.java
@@ -1,12 +1,24 @@
 package com.marcelodev.movieflix.service;
 
+import com.marcelodev.movieflix.entity.Category;
+import com.marcelodev.movieflix.exception.CategoryException;
 import com.marcelodev.movieflix.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()) {
+            throw new CategoryException("there are no categories");
+        }
+        return categories;
+    }
 }
