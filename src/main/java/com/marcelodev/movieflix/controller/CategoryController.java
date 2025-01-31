b0f1d5960b4a2d1800fc91efdc16da387015451a
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-01-31 20:32:32 -0300

    creates category findall method in controller

--- src/main/java/com/marcelodev/movieflix/controller/CategoryController.java
+++ src/main/java/com/marcelodev/movieflix/controller/CategoryController.java
@@ -1,10 +1,16 @@
 package com.marcelodev.movieflix.controller;
 
+import com.marcelodev.movieflix.controller.response.CategoryResponse;
+import com.marcelodev.movieflix.entity.Category;
+import com.marcelodev.movieflix.mapper.CategoryMapper;
 import com.marcelodev.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,5 +19,10 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> findAll() {
+        List<CategoryResponse> categoryResponses = categoryService.findAll()
+                .stream().map(CategoryMapper::toCategoryResponse).toList();
+        return ResponseEntity.ok(categoryResponses);
+    }
 }
