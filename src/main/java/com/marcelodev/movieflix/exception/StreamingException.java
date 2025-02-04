8a16a4cf014ace717442078ca1e3f6be731d7ff7
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-03 21:16:55 -0300

    created streaming exception

---
+++ src/main/java/com/marcelodev/movieflix/exception/StreamingException.java
@@ -0,0 +1,8 @@
+package com.marcelodev.movieflix.exception;
+
+public class StreamingException extends RuntimeException{
+
+    public StreamingException(String message) {
+        super(message);
+    }
+}
