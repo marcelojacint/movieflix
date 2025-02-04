c9bec2a9ef02f1c1812e043749955c6137f95de6
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-03 21:07:37 -0300

    created streaming repository

---
+++ src/main/java/com/marcelodev/movieflix/repository/StreamingRepository.java
@@ -0,0 +1,9 @@
+package com.marcelodev.movieflix.repository;
+
+import com.marcelodev.movieflix.entity.Streaming;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StreamingRepository extends JpaRepository<Streaming, Long> {
+}
