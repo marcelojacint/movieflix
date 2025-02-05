b365133adcbba727b42ff8bd08cf294929914a86
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-04 21:41:51 -0300

    feat: created streaming findAll method in service and controller

--- src/main/java/com/marcelodev/movieflix/controller/StreamingController.java
+++ src/main/java/com/marcelodev/movieflix/controller/StreamingController.java
@@ -1,9 +1,19 @@
 package com.marcelodev.movieflix.controller;
 
+import com.marcelodev.movieflix.controller.response.StreamingResponse;
+import com.marcelodev.movieflix.entity.Streaming;
+import com.marcelodev.movieflix.mapper.StreamingMapper;
 import com.marcelodev.movieflix.service.StreamingService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class StreamingController {
 
     private final StreamingService streamingService;
+
+    @GetMapping
+   public ResponseEntity<List<StreamingResponse>> findAll() {
+        List<StreamingResponse> streamingResponses = streamingService.findAll()
+                .stream()
+                .map(StreamingMapper::toStreamingResponse).toList();
+        return ResponseEntity.ok(streamingResponses);
+    }
 }
--- src/main/java/com/marcelodev/movieflix/service/StreamingService.java
+++ src/main/java/com/marcelodev/movieflix/service/StreamingService.java
@@ -1,12 +1,24 @@
 package com.marcelodev.movieflix.service;
 
+import com.marcelodev.movieflix.entity.Streaming;
+import com.marcelodev.movieflix.exception.StreamingException;
 import com.marcelodev.movieflix.repository.StreamingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class StreamingService {
 
     private StreamingRepository streamingRepository;
+
+    public List<Streaming> findAll() {
+         if(streamingRepository.findAll().isEmpty()) {
+             throw new StreamingException("No streaming found");
+         }
+         return streamingRepository.findAll();
+    }
+
 }
