1e7cc526671fa246d5857ee653cadadb9a332e1d
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-09 12:13:05 -0300

    feat(movie): created findAll method in service and controller

--- src/main/java/com/marcelodev/movieflix/controller/MovieController.java
+++ src/main/java/com/marcelodev/movieflix/controller/MovieController.java
@@ -1,9 +1,15 @@
 package com.marcelodev.movieflix.controller;
 
+import com.marcelodev.movieflix.controller.response.MovieResponse;
+import com.marcelodev.movieflix.mapper.MovieMapper;
 import com.marcelodev.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
     private final MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        List<MovieResponse> movieResponses = movieService.findAll()
+                .stream()
+                .map(MovieMapper::toMovieResponse).toList();
+        return ResponseEntity.ok(movieResponses);
+    }
 }
--- src/main/java/com/marcelodev/movieflix/service/MovieService.java
+++ src/main/java/com/marcelodev/movieflix/service/MovieService.java
@@ -1,13 +1,24 @@
 package com.marcelodev.movieflix.service;
 
+import com.marcelodev.movieflix.entity.Movie;
+import com.marcelodev.movieflix.exception.StreamingException;
 import com.marcelodev.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class MovieService {
 
     private MovieRepository movieRepository;
+
+    public List<Movie> findAll() {
+        if(movieRepository.findAll().isEmpty()) {
+            throw new StreamingException("No movies found");
+        }
+        return movieRepository.findAll();
+    }
 
 }
