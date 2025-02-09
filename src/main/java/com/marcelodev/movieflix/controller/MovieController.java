73c5c8f0f410501cadb5b102349071aa55648c49
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-09 12:47:46 -0300

    feat(Movie): created findById method in service and controller

--- src/main/java/com/marcelodev/movieflix/controller/MovieController.java
+++ src/main/java/com/marcelodev/movieflix/controller/MovieController.java
@@ -6,6 +6,7 @@ import com.marcelodev.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,12 @@ public class MovieController {
                 .stream()
                 .map(MovieMapper::toMovieResponse).toList();
         return ResponseEntity.ok(movieResponses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findById(@PathVariable Long id) {
+      return movieService.findById(id)
+              .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+              .orElse(ResponseEntity.notFound().build());
     }
 }
