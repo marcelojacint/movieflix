6ba6d3b7e1e7932b829b5f6587ac35d28b502935
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-09 12:21:37 -0300

    feat: created movieExpection

---
+++ src/main/java/com/marcelodev/movieflix/exception/MovieException.java
@@ -0,0 +1,8 @@
+package com.marcelodev.movieflix.exception;
+
+public class MovieException extends RuntimeException {
+
+    public MovieException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/marcelodev/movieflix/service/MovieService.java
+++ src/main/java/com/marcelodev/movieflix/service/MovieService.java
@@ -1,12 +1,14 @@
 package com.marcelodev.movieflix.service;
 
 import com.marcelodev.movieflix.entity.Movie;
+import com.marcelodev.movieflix.exception.MovieException;
 import com.marcelodev.movieflix.exception.StreamingException;
 import com.marcelodev.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,9 +18,16 @@ public class MovieService {
 
     public List<Movie> findAll() {
         if(movieRepository.findAll().isEmpty()) {
-            throw new StreamingException("No movies found");
+            throw new MovieException("No movies found");
         }
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id) {
+        if(!movieRepository.existsById(id)) {
+            throw new MovieException("Movie not found");
+        }
+        return movieRepository.findById(id);
     }
 
 }
