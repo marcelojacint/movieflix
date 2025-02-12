769df8c33aa7409252e436dcbb6cdb4cdf50b4bc
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-12 12:14:26 -0300

    feat(Movie): created findMovieByCategories method in repository

--- src/main/java/com/marcelodev/movieflix/repository/MovieRepository.java
+++ src/main/java/com/marcelodev/movieflix/repository/MovieRepository.java
@@ -1,10 +1,15 @@
 package com.marcelodev.movieflix.repository;
 
+import com.marcelodev.movieflix.entity.Category;
 import com.marcelodev.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+   List<Movie> findMovieByCategories(List<Category> categories);
 
 }
