e9cf4843f0619e618a21d20baae02e2ec681cddb
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-13 11:48:49 -0300

    feat: created User entity

---
+++ src/main/java/com/marcelodev/movieflix/entity/User.java
@@ -0,0 +1,26 @@
+package com.marcelodev.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+
+}
