d25671b544727066235c9ce372f6a7eb2eb58798
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-18 12:11:29 -0300

    refactor(UserService): implementing password encryption with PasswordEncoder

--- src/main/java/com/marcelodev/movieflix/service/UserService.java
+++ src/main/java/com/marcelodev/movieflix/service/UserService.java
@@ -3,6 +3,7 @@ package com.marcelodev.movieflix.service;
 import com.marcelodev.movieflix.entity.User;
 import com.marcelodev.movieflix.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,7 +12,10 @@ public class UserService {
 
     private UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
